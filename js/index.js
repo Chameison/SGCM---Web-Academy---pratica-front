8863a5f2d5259a4afbcfb2718a4b8d1e3881d1a5
Chameison Araujo <chameison.acre@gmail.com>
2022-08-29 17:40:56 -0500

    Colocando alguns elementos no js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ window.onload = function() {
     botoes.forEach(item => {
         item.addEventListener("click", () => {
             if(confirm("Deseja realmente excluir ?")){
-                alert("excluir");
-            };
+                item.parentNode.parentNode.remove(); //Manipulação de DOM - fazer alterações no htm /css 
+            }
         })
     });//pegar cada elemento dentro dos botoes e executar o bloco de codigos que vou estar fazendo
     //funcoes anonimas
