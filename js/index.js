2ffb1418f49b42c53947c511d2f7ab4d9bd8d7b8
Chameison Araujo <chameison.acre@gmail.com>
2022-08-30 18:31:33 -0500

    Adicionando linha - js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ window.onload = function() {
     //funcoes anonimas
     //funcoes lambda -- hero functions 
 }
+let xhr = new XMLHttpRequest(); //
+let botaoCarregar = document.querySelector("a#load");
+if (botaoCarregar) {
+    botaoCarregar.addEventListener("click", () => {
+        let tabela = document.querySelector("table");
+        let url = "http://my-json-server.typicode.com/danielnsilva/json/profissionais";
+        xhr.open("GET", url); //metodos usamos quando queremos fazer certa funcção,  
+        xhr.addEventListener("readystatechange", () => {
+            if (xhr.readyState == 4 && xhr.status == 200){
+                let dados = JSON.parse(xhr.responseText);
+                for(let item in dados) {
+                    let linha = document.createElement("tr"); //criando a linha
+                    let id = document.createElement("td"); 
+                    let nome = document.createElement("td");
+                    let registro = document.createElement("td");
+                    let especialidade = document.createElement("td");
+                    let unidade = document.createElement("td");
+                    let telefone = document.createElement("td");
+                    let email = document.createElement("td");
+                    let acoes = document.createElement("td");
+                }
+            }
+        });
+    })
+}
