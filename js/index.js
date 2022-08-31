afd036fd55342dd253b300215d5aca76da5b4083
Chameison Araujo <chameison.acre@gmail.com>
2022-08-30 19:51:54 -0500

    finalizando fundamentos front-end

--- js/index.js
+++ js/index.js
@@ -10,29 +10,59 @@ window.onload = function() {
     });//pegar cada elemento dentro dos botoes e executar o bloco de codigos que vou estar fazendo
     //funcoes anonimas
     //funcoes lambda -- hero functions 
-}
-let xhr = new XMLHttpRequest(); //
-let botaoCarregar = document.querySelector("a#load");
-if (botaoCarregar) {
-    botaoCarregar.addEventListener("click", () => {
-        let tabela = document.querySelector("table");
-        let url = "http://my-json-server.typicode.com/danielnsilva/json/profissionais";
-        xhr.open("GET", url); //metodos usamos quando queremos fazer certa funcção,  
-        xhr.addEventListener("readystatechange", () => {
-            if (xhr.readyState == 4 && xhr.status == 200){
-                let dados = JSON.parse(xhr.responseText);
-                for(let item in dados) {
-                    let linha = document.createElement("tr"); //criando a linha
-                    let id = document.createElement("td"); 
-                    let nome = document.createElement("td");
-                    let registro = document.createElement("td");
-                    let especialidade = document.createElement("td");
-                    let unidade = document.createElement("td");
-                    let telefone = document.createElement("td");
-                    let email = document.createElement("td");
-                    let acoes = document.createElement("td");
-                }
-            }
+
+
+    //let xhr = new XMLHttpRequest(); //
+    let botaoCarregar = document.querySelector("a#load");
+    if (botaoCarregar) {
+        botaoCarregar.addEventListener("click", () => {
+            let tabela = document.querySelector("table");
+            let url = "http://my-json-server.typicode.com/danielnsilva/json/profissionais";
+          //  xhr.open("GET", url); //metodos usamos quando queremos fazer certa funcção,  
+        //    xhr.addEventListener("readystatechange", () => {
+               // if (xhr.readyState == 4 && xhr.status == 200){
+                //    let dados = JSON.parse(xhr.responseText);
+            fetch(url)
+            .then(resposta => resposta.json())//controlar o fluxo de informação
+            .then(dados => {
+                    for(let item in dados) {
+                        let linha = document.createElement("tr"); //criando a linha
+                        let id = document.createElement("td"); 
+                        let nome = document.createElement("td");
+                        let registro = document.createElement("td");
+                        let especialidade = document.createElement("td");
+                        let unidade = document.createElement("td");
+                        let telefone = document.createElement("td");
+                        let email = document.createElement("td");
+                        let acoes = document.createElement("td");
+                        id.classList.add("fit");
+                        id.textContent = dados[item].id;
+                        nome.textContent = dados[item].nome;
+                        registro.textContent = dados[item].registro;
+                        especialidade.textContent = dados[item].especialidade;
+                        unidade.textContent = dados[item].unidade;
+                        telefone.textContent = dados[item].telefone;
+                        email.textContent = dados[item].email;
+                        acoes.innerHTML = `<a href="javascript:void(0)" class="botao">Editar</a> \n
+                        <a href="javascript:void(0)" class="botao excluir">Excluir</a> `;
+                        linha.appendChild(id);
+                        linha.appendChild(nome);
+                        linha.appendChild(registro);
+                        linha.appendChild(especialidade);
+                        linha.appendChild(unidade);
+                        linha.appendChild(telefone);
+                        linha.appendChild(email);
+                        linha.appendChild(acoes);
+
+                        tabela.tBodies[0].appendChild(linha); //adicionandoo a linha no tbody da tabela no primeiro termo = 0
+
+                    }
+                })
+              //  }
+          //  });
+            
+          //  xhr.send();
         });
-    })
+    }
+    
 }
